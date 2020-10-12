967af7520eb5370bf64db9692f1217fe0666172d
Rhone Lachner <fernandclay@gmail.com>
2020-10-12 16:15:52 -0700

    added data.js array

---
+++ data.js
@@ -0,0 +1,49 @@
+/* eslint-disable */
+
+export const pokemonData = [
+    {
+    "name":"squirtle",
+    "id":10,
+    "height":5,
+    "weight":90,
+    "type":"water",
+    "hp":44,
+    "special_attack":50,
+    "special_defense":64,
+    "speed":43,
+    "ability_1":"torrent",
+    "ability_hidden":"rain-dish",
+    "url_image":"http://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+    },
+     
+    {
+    "name":"charmander",
+    "id":5,
+    "species_id":4,
+    "height":6,
+    "weight":85,
+    "type":"fire",
+    "hp":39,
+    "special_attack":60,
+    "special_defense":50,
+    "speed":65,
+    "ability_1":"blaze",
+    "url_image":"http://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+    },
+    
+    {
+    "pokemon":"bulbasaur",
+    "id":1,
+    "species_id":1,
+    "height":7,
+    "weight":69,
+    "base_experience":64,
+    "type_1":"grass + poison",
+    "hp":45,
+    "special_attack":65,
+    "special_defense":65,
+    "speed":45,
+    "ability":"overgrow + chlorophyll",
+    "url_image":"http://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+    },
+]
